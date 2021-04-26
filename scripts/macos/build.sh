bf1476cbd2a5be8ed5e890e91d0ac04c5839747b
Adriel Paredes <adriel.paredes@gmail.com>
2021-04-26 17:31:52 -0300

    Added Macos app creationg and DMG creation

---
+++ scripts/macos/build.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+
+APP=target/Kogito.app
+CONTENTS=$APP/Contents
+MACOS=$CONTENTS/MacOs
+RESOURCES=$CONTENTS/Resources
+NAME=Kogito
+DMG=$NAME.dmg
+APPLICATIONS=target/Applications
+
+
+rm -rf target
+rm -rf Kogito.dmg
+
+mkdir -p $CONTENTS
+mkdir $MACOS
+mkdir $RESOURCES
+
+cp ../../build/darwin/kogito $MACOS
+cp src/Info.plist $CONTENTS
+cp src/Kogito.png $RESOURCES
+ln -s /Applications $APPLICATIONS
+
+hdiutil create /tmp/tmp.dmg -ov -volname $NAME -fs HFS+ -srcfolder "target" 
+hdiutil convert /tmp/tmp.dmg -format UDZO -o Kogito.dmg
